122ab212551ac6d7517eeaa363a7fec89e6dad60
Brian Thomas <bthomas@imaginuity.com>
2020-09-15 11:47:18 -0500

    updating colors

--- js/script.js
+++ js/script.js
@@ -22,11 +22,11 @@ function setTheme(mode) {
   if (mode === "blue") {
     document.getElementById("theme-style").href = "../css/blue-theme.css";
   }
-  if (mode === "green") {
-    document.getElementById("theme-style").href = "../css/green-theme.css";
+  if (mode === "orange") {
+    document.getElementById("theme-style").href = "../css/orange-theme.css";
   }
-  if (mode === "purple") {
-    document.getElementById("theme-style").href = "../css/purple-theme.css";
+  if (mode === "red") {
+    document.getElementById("theme-style").href = "../css/red-theme.css";
   }
 
   localStorage.setItem("theme", mode);
